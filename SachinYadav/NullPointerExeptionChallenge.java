13601b228da2e87b9fdb81d2af0c37f21149f07d
Sachin Yadav <147396495+sachinyadav20187@users.noreply.github.com>
2023-10-17 11:01:19 +0530

    Bug fix
    NullPointerExceptionChallenge fixed

---
+++ SachinYadav/NullPointerExeptionChallenge.java
@@ -0,0 +1,24 @@
+package Exercise2;
+
+/*
+ * This is debugging exercise-02 of project earlybird
+ * The corrected code after debugging and adding exception handling
+ * @author - Sachin Yadav
+ * @Organization - Veersa Technologies
+ */
+public class NullPointerExceptionChallenge {
+
+    public static void main(String[] args) {
+        // String str = "Sachin" it will work fine because the string is not null
+        // String str = null; // gives NullPointerExepception because str is empty 
+        String str = "Sachin";
+        try {
+            int length = str.length();
+            System.out.println("Length of the string: " + length);
+        } catch (NullPointerException nullPointerException) {
+            System.out.println("String is null. Please provide the non-null string");
+        }
+
+
+    }
+}
