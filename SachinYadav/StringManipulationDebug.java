6a690cfc84342556e9a22021b32d617f006b6d48
Sachin Yadav <147396495+sachinyadav20187@users.noreply.github.com>
2023-10-18 11:54:43 +0530

    Bug fix
    StringManipulationDebug fixed

---
+++ SachinYadav/StringManipulationDebug.java
@@ -0,0 +1,26 @@
+package Exercise3;
+
+/*
+ * This is Exercise 3 of debugging
+ * The corrected code after debugging
+ * @Author - Sachin Yadav
+ * @Organization - Veersa Technologies
+ */
+public class StringManipulationDebug {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "world";
+
+        // Concatenating two strings
+        String result = str1 + str2;
+        System.out.println("Concatenation result: " + result);
+
+        // Finding the substring
+        String sub = result.substring(1); // Java substring takes the end index exclusive so, no need to mention the end index.
+        System.out.println("Substring result: " + sub);
+
+        // Changing the case
+        String lowerCase = result.toLowerCase();
+        System.out.println("Upper case result: " + lowerCase);
+    }
+}
