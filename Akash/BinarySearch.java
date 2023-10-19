571095fec72a1192ffffb1c71892979d80a887a0
rest-controller <148121203+rest-controller@users.noreply.github.com>
2023-10-19 10:12:09 +0530

    Create BinarySearch.java

---
+++ Akash/BinarySearch.java
@@ -0,0 +1,43 @@
+package com.example.cruddto.service;
+
+public class BinarySearch {
+
+    /**
+     *
+     * @param arr the array
+     * @param target the search element
+     * @return the index if found, -1 iif not found
+     */
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[mid] > target) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 8, 12, 16, 23, 38, 45, 56, 72, 91};
+        int target = 23;
+        int result = binarySearch(arr, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index " + result);
+        } else {
+            System.out.println("Element not found in the array");
+        }
+    }
+}
