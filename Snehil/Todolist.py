90e66c826dbaed8b710b4abebd31996a77ccedb9
SnehilSatyamok <148181468+SnehilSatyamok@users.noreply.github.com>
2023-10-22 21:55:32 +0530

    Update Todolist.py

--- Snehil/Todolist.py
+++ Snehil/Todolist.py
@@ -28,3 +28,42 @@ class ToDoList:
         print("To-Do List:")
         for i, task in enumerate(self.tasks):
             print(f"{i}. {task}")
+
+def main():
+    todo_list = ToDoList()
+    while True:
+        print("\nSelect an option:")
+        print("1. Add a task")
+        print("2. Edit a task")
+        print("3. Remove a task")
+        print("4. Display tasks")
+        print("5. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            task = input("Enter task to add: ")
+            todo_list.add_task(task)
+
+        elif choice == '2':
+            index = int(input("Enter index of task to edit: "))
+            new_task = input("Enter new task: ")
+            todo_list.edit_task(index, new_task)
+
+        elif choice == '3':
+            index = int(input("Enter index of task to remove: "))
+            todo_list.remove_task(index)
+
+        elif choice == '4':
+            todo_list.display_tasks()
+
+        elif choice == '5':
+            print("Exiting the to-do list application.")
+            break
+
+        else:
+            print("Invalid choice. Please select a valid option.")
+
+
+if __name__ == "__main__":
+    main()
